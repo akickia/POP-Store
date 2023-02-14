2054942658e0a59a38247f6df1c493a8986eee33
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-02-14 17:00:11 +0100

    first commit - kind of late in the process

---
+++ script.js
@@ -0,0 +1,61 @@
+import { addToCart, renderCart, expandCart } from "./cart.js";
+
+async function getPopcorns() {
+  try {
+    let data = await fetch("products.json");
+    data = await data.json()
+    generateItems(data)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+getPopcorns()
+expandCart()
+
+
+
+function generateItems(data) {
+  let storeContainer = document.querySelector(".store-container")
+  data.forEach(item => {
+    let newSection = document.createElement("section")
+    newSection.setAttribute("class", "card")
+    newSection.innerHTML = `
+    <img src="img/${item.img}" alt="${item.name}">
+    <h4>${item.name}</h4>
+    <p class="info">${item.desc}</p>
+    <p class="price">${item.pricePerHekto}kr/hg</p>
+    `
+    storeContainer.appendChild(newSection)
+    newSection.addEventListener("click", () => {
+      generateConfirmationModule(item)
+    })
+  })
+}
+
+
+
+function generateConfirmationModule(item) {
+  let confirmationModule = document.querySelector(".info-module")
+  let confirmationModuleSection = document.querySelector(".info-module section")
+  confirmationModule.style.display = "block"
+  confirmationModuleSection.innerHTML = `
+
+  <h1>Vill du lägga till ${item.name} i kundvagnen?</h1>
+  <h4 class="closeBtn">X</h4>
+  <div>
+  <button class="choiseYes">JA</button>
+  <button class="choiseNo">NEJ</button>
+  </div>
+  `
+  let closeBtn = document.querySelector(".closeBtn")
+  let closeBtn2 = document.querySelector(".choiseNo")
+  closeBtn.addEventListener("click", () => {
+    confirmationModule.style.display = "none"
+  })
+  closeBtn2.addEventListener("click", () => {
+    confirmationModule.style.display = "none"
+  })
+  addToCart(item)
+}
